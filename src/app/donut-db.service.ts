0a18fece3b3ced557578c1d41cd307d4b385f7f1
miraj <miraj00@gmail.com>
2023-03-09 23:33:29 -0500

    services edited for urlbase and httpClient to Get json from url

--- src/app/donut-db.service.ts
+++ src/app/donut-db.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Donut } from './donut';      // importing interface
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DonutDbService {
 
-  constructor() { }
+  urlbase:string = "https://grandcircusco.github.io/demo-apis/donuts.json";
+  constructor(private httpClient:HttpClient) {
+
+   }
+
+   getDonut() : Observable<Donut> {
+    return this.httpClient.get<Donut>(this.urlbase);
+
+  }
 }
--- src/app/people-db.service.ts
+++ src/app/people-db.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { People } from './people';      // importing interface
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class PeopleDbService {
 
-  constructor() { }
+  urlbase:string = "https://grandcircusco.github.io/demo-apis/computer-science-hall-of-fame.json";
+  constructor(private httpClient:HttpClient) {
+
+   }
+
+   getDonut() : Observable<People> {
+    return this.httpClient.get<People>(this.urlbase);
+
+  }
+
 }
