b3c3de809f86d2ff0c7d4992b1589788cbf8f48c
miraj <miraj00@gmail.com>
2023-03-09 21:59:05 -0500

    Donuts and FamousPeople Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DonutsComponent } from './donuts/donuts.component';
+import { FamousPeopleComponent } from './famous-people/famous-people.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonutsComponent,
+    FamousPeopleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/donuts/donuts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donuts',
+  templateUrl: './donuts.component.html',
+  styleUrls: ['./donuts.component.css']
+})
+export class DonutsComponent {
+
+}
